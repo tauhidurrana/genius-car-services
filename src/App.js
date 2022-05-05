81320d059c464082a8ea9e58ff10d6b3051bb807
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-05-05 10:49:36 +0600

    mongodb database added and Add service added ,user can add service that will add to database

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
 import Checkout from './Pages/CheckOut/Checkout';
 import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
+import AddService from './Pages/AddService/AddService';
 
 function App() {
   return (
@@ -23,11 +24,15 @@ function App() {
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+        
         <Route path='/checkout' element={
           <RequireAuth>
             <Checkout></Checkout>
           </RequireAuth>
         }></Route>
+        
+        <Route path='/addservice' element={<AddService></AddService>}></Route>
+        
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
     const {serviceID} = useParams()
+    const [service, setService] = useState({});
+    useEffect(()=>{
+        const url = `http://localhost:5000/service/${serviceID}`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>setService(data))
+    },[])
     return (
         <div className='container text-center'>
-            <h2>Service Detail: {serviceID}</h2>
+            <h2>Service Detail: {service.name}</h2>
             <Link to='/checkout'>
                 <button className='btn btn-primary'>Checkout</button>
             </Link>
